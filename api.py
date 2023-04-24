cb1c647c8845dca196ab9a75260e04338872b8c1
David Colmenares <lcolmenares@skandia.com.co>
2023-04-24 03:17:32 -0500

    v.1

--- api.py
+++ api.py
@@ -2,14 +2,16 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import numpy as np
 import pandas as pd
+import json
 from tokenGenerator import tokenApi
 similarity_matrix = np.loadtxt("similarity_matrix.csv", delimiter=",")
 
-user = {"a": [similarity_matrix, similarity_matrix, similarity_matrix],
-        "b": [similarity_matrix, similarity_matrix, similarity_matrix],
-        "c": [similarity_matrix, similarity_matrix, similarity_matrix]}
+user = {"a": [],
+        "b": [],
+        "c": []}
 
 
+tags = pd.read_csv("keywords.csv" )
 movies_df = pd.read_csv("movies_all_data.csv")
 
 app = Flask(__name__)
@@ -19,23 +21,22 @@ CORS(app)
 @app.route('/<token>')
 def hello_world(token):
     ini = 1
-    end = 5
-    movie_data = []
-
-    if 'token' in request.view_args and len(user[token]) > 0:
-        for item in user[token]:
-            id_of_movie = movies_df[movies_df['title']=='Toy Story'].index[0]
-            distances = similarity_matrix[id_of_movie]
-            movie_list = sorted(list(enumerate(distances)),
-                                reverse=True, key=lambda x: x[1])[1:5]
-            # ini += 1
-            # end += end
-            # distances2lvl = item
-            # movie_list.append = sorted(list(enumerate(distances2lvl)),
-            #                            reverse=True, key=lambda x: x[6])[6:10]
-            # distances3lvl = item
-            # movie_list.append = sorted(list(enumerate(distances3lvl)),
-            #                            reverse=True, key=lambda x: x[10])[10:5]
+    end = 5 
+    movie_data = [] 
+    if 'token' in request.view_args  :
+        tagsMovie =json.loads(tags[tags['id'] == 862]["keywords"][0].replace("'", "\""))  
+        for item in tagsMovie :
+            if  len(user[token])>0:
+                for savetags in user[token]:
+                    if  item['name']  == savetags["tag"]:   
+                        savetags["count"]+=1 
+                        break 
+            else:
+                user[token].append({"count":0,"tag":item['name']}) 
+        
+        distances = similarity_matrix[862]
+        movie_list = sorted(list(enumerate(distances)),
+                reverse=True, key=lambda x: x[1])[1:5]
     else:
         distances = similarity_matrix[862]
         movie_list = sorted(list(enumerate(distances)),
