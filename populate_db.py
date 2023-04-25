2c20d3d3feb607617ea92b126099b03b2b9b0902
lcolmenares <lcolmenares@Skandia.com>
2023-04-25 01:11:36 -0500

    add populate

--- populate_db.py
+++ populate_db.py
@@ -23,18 +23,17 @@ client.schema.create_class(
 )
 
 movies_data = pd.read_csv("movies_all_data.csv")
-movies_data.columns = ["id", "title", "tags"]
-movies_data["vector"] = 0
+movies_data.columns = ["id", "title", "tags","vector"]
 
 
-def generate_embeding(row):
-    result = openai.Embedding.create(
-        input=[row["tags"]], engine="embedding-eagle-dev")
-    row["vector"] = result.data[0].embedding
+async def generate_embeding(i):
+    result = await openai.Embedding.acreate(
+        input=[movies_data.at[i, "tags"]], engine="embedding-eagle-dev")
+    movies_data.at[i, "vector"] = result.data[0].embedding
     return result.data[0].embedding
 
 
-tasks = [generate_embeding(row) for i, row in movies_data.iterrows()]
+tasks = [generate_embeding(i) for i, row in movies_data.iterrows()]
 
 
 async def main():
@@ -44,7 +43,7 @@ async def main():
     for i in range(0, len(tasks), max_query):
         result = await asyncio.gather(*tasks[i:i+max_query])
         embeddings_response.extend(result)
-        print(f"voy en la linea {i} que es {result[0]['title']}")
+        print(f"voy en la linea {i} ")
         await asyncio.sleep(delay_per_request)
 
     counter = 0
